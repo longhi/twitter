70308d2e156fdd04b153688fe94757a604b626db
nico <longhiv27@gmail.com>
2017-11-20 06:03:40 -0500

    añadiendo js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+var insert-text = document.getElementById('write');
+write.addEventListener("keydown", count);
 
+function count() {
+    var sizeText =document.getElementById("size-text");
+    var limit= 140;
+    var length = document.getElementById("write").value.length;
+    document.getElementById("size-text").innerHTML= limit-length;
+
+    if(length>=140){
+        conter.style.color="green";
+        button.disabled=true;
+    }
+    else if(length>=130){
+        conter.style.color="blue";
+        button.disabled=true;
+    }
+    else if(length>=120){
+        conter.style.color="orange";
+        button.disabled=false;
+    }
+    else{
+        conter.style.color="orange";
+        button.disabled = false;
+    }
+};
+var button = document.getElementById("button");
+button.disabled= true;
+button.addEventListener("click",submit);
